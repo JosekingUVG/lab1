bb841237dee8bba97d9026068422389a00365990
JoseKingUVG <riv24376@uvg.edu.gt>
2026-07-29 16:34:43 -0600

    poligono 4

--- src/main.zig
+++ src/main.zig
@@ -30,13 +30,14 @@ pub fn main() !void {
     // Polígono
     // -----------------------------------------
 
-    const polygon1 = fb.Polygon{
-        .vertices = &[_]fb.Vec2{
-            .{ .x = 377, .y = 249 },
-            .{ .x = 411, .y = 197 },
-            .{ .x = 436, .y = 249 },
-        },
-    };
+    const polygon1 = fb.Polygon{ .vertices = &[_]fb.Vec2{
+        .{ .x = 413, .y = 177 }, .{ .x = 448, .y = 159 }, .{ .x = 502, .y = 88 },  .{ .x = 553, .y = 53 },  .{ .x = 535, .y = 36 },  .{ .x = 676, .y = 37 },  .{ .x = 660, .y = 52 },
+        .{ .x = 750, .y = 145 }, .{ .x = 761, .y = 179 }, .{ .x = 672, .y = 192 }, .{ .x = 659, .y = 214 }, .{ .x = 615, .y = 214 }, .{ .x = 632, .y = 230 }, .{ .x = 580, .y = 230 },
+        .{ .x = 597, .y = 215 }, .{ .x = 552, .y = 214 }, .{ .x = 517, .y = 144 }, .{ .x = 466, .y = 180 },
+    } };
+    const polygon2 = fb.Polygon{ .vertices = &[_]fb.Vec2{
+        .{ .x = 682, .y = 175 }, .{ .x = 708, .y = 120 }, .{ .x = 735, .y = 148 }, .{ .x = 739, .y = 170 },
+    } };
 
     // -----------------------------------------
     // Bucle principal
@@ -50,13 +51,25 @@ pub fn main() !void {
         // Dibujar polígono
         framebuffer.drawPolygon(
             polygon1,
-            rl.Color.green,
+            rl.Color.white,
         );
 
         // Rellenar poligono
         framebuffer.fillPolygon(
             polygon1,
             rl.Color.green,
+        );
+
+        // Dibujar polígono
+        framebuffer.drawPolygon(
+            polygon2,
+            rl.Color.white,
+        );
+
+        // Rellenar poligono con color de fondo
+        framebuffer.fillPolygon(
+            polygon2,
+            rl.Color.black,
         );
 
         // Convertir Image -> Texture
